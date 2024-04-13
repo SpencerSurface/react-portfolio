f0965d6cdf4f61332e8204007f20357e4aff37af
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-13 12:23:42 -0400

    create App component, import components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom"
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+import "./App.css"
 
 function App() {
-    return ();
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App
