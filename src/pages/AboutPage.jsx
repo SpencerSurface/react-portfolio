7b76e71ba98ea7d59a6cabd278ee19989716cb1c
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-14 12:55:13 -0400

    write About section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,16 @@
+import headshot from "../assets/headshot.png"
+
 export default function AboutPage() {
     return (
-        // TODO: write about page
-        <div>about</div>
+        <>
+            <h2>About Me</h2>
+            <img src={headshot} alt="Headshot of Specner Surface"></img>
+            <p>
+                Software developer focusing on full-stack web development. Earned a Bachelor of Science in Computer Science and Engineering from Ohio State University. Problem solving innovator who is passionate about developing apps that are correct and performant. Strengths include attention to detail, analytical thinking, and teamwork.
+            </p>
+            <p>
+                
+            </p>
+        </>
     )
 }
