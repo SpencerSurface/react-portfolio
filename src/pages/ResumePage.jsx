7892b67d0a608742ea469be05f64a73f01c2baa0
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-14 16:39:46 -0400

    write Resume Page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,6 +1,31 @@
+import resume from "../assets/resume.pdf"
+
 export default function ResumePage() {
     return (
-        // TODO: write resume page
-        <div>resume</div>
+        <>
+            <h3>Resume</h3>
+            <p>Download <a href={resume} download="spencer-surface-resume.pdf">my resume</a></p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Responsive Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+                <li>Progressive Web Apps</li>
+            </ul>
+            <h3>Back-end Proficiences</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </>
     )
 }
