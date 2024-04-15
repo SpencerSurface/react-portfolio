8626da8ce6a015c2ebfafd3f1a37cdb18bf582de
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-15 00:15:51 -0400

    implement footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+// import { faGithub } from '@fortawesome/free-solid-svg-icons'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 function Footer() {
     return (
         <footer>
-            {/* TODO: add social media links */}
+            <a href="https://www.linkedin.com/in/spencersurface/">
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="https://github.com/SpencerSurface">
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="mailto:spencer.surface@gmail.com">
+                <FontAwesomeIcon icon={faEnvelope} />
+            </a>
         </footer>
     );
 }
