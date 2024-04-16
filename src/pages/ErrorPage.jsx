c4ee73df990c477d24da1d5885aa20e29d74d416
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-04-16 18:09:10 -0400

    implement error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,8 @@
 export default function ErrorPage() {
     return (
-        // TODO: write error page
-        <div>error</div>
+        <div className="p-4">
+            <h2>Error</h2>
+            <p>That page does not exist! Go <a href="/">home</a>?</p>
+        </div>
     )
 }
